b5c26b9b1d7103d2455217c52c5b1c93e2ba02b0
Kwonkunkun <kunwoo5012@naver.com>
2021-12-01 00:57:30 +0900

    프로그래밍 language, pampam app 추가

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -6,101 +6,106 @@ import ProjectCard from "./project-card";
 import Typography from "@material-ui/core/Typography";
 
 const projects = [
-    {
-        title: "globlin",
-        info:
-            "React, Redux, Redux-saga를 이용하여 인플루언서 마케팅에 필요한 기능들을 구현",
-        img:
-            "https://user-images.githubusercontent.com/59603575/124921130-0bbfb880-e033-11eb-8cc3-ad051d3b24ac.png",
-        gitUrl: "https://github.com/Kwonkunkun/globlin_guide",
-    },
-    {
-        title: "simple_particle",
-        info: "particle 효과, 비 효과를 html canvas, 바닐라js로 구현",
-        img:
-            "https://user-images.githubusercontent.com/59603575/103346337-0e1ae600-4ad7-11eb-8b23-e5f618819610.gif",
-        gitUrl: "https://github.com/Kwonkunkun/Simple_particle",
-    },
-    {
-        title: "youtube_clone",
-        info: "Youtube api, react를 이용한 youtube 클론코딩 프로젝트",
-        img:
-            "https://user-images.githubusercontent.com/59603575/105004700-acb3ba80-5a77-11eb-90f5-d29bb14a529c.png",
-        gitUrl: "https://github.com/Kwonkunkun/React_youtube",
-    },
-    {
-        title: "cardmaker",
-        info: "react, firebase를 이용한 카드를 만들수 있는 프로젝트",
-        img:
-            "https://user-images.githubusercontent.com/59603575/105945809-5400a500-60a9-11eb-96f3-7c2363c35aad.gif",
-        gitUrl: "https://github.com/Kwonkunkun/React_cardmaker",
-    },
-    {
-        title: "온고지신-국궁(VR-BowAndArrow)",
-        info: "VR을 이용한 교육적 목적의 국궁 체험 시뮬레이션 콘텐츠",
-        img:
-            "https://user-images.githubusercontent.com/59603575/102003822-a9325400-3cbf-11eb-8367-af01291f9b33.png",
-        gitUrl: "https://github.com/Kwonkunkun/VR-BowAndArrow",
-    },
-    {
-        title: "VR-DrawAndPainting",
-        info:
-            "VR상에서 그림을 그려 딥러닝 모델로 판별하고 다시 VR환경에 관련 오브젝트를 띄운다. 이후, 오브젝트로 색칠하며 전시하는 프로젝트",
-        img:
-            "https://user-images.githubusercontent.com/59603575/102004661-a3d90780-3cc7-11eb-894c-fd19d713e5a1.png",
-        gitUrl: "https://github.com/Kwonkunkun/DrawAndPainting_VR",
-    },
-    {
-        title: "제3의 눈(AR 전시)",
-        info:
-            "서울문화재단 청년예술청 지원사업 - <스페이스랩:아직> 선정작품 , AR 전시 개발자로 참여, 뇌파센서와 연동하여 뇌파값에 따라 오브젝트 변화",
-        img:
-            "https://user-images.githubusercontent.com/59603575/102004337-9cfcc580-3cc4-11eb-93ce-d2bdaba81a20.png",
-        gitUrl: "https://github.com/Kwonkunkun/AR_Exhibition",
-    },
+  {
+    title: "pampam",
+    info:
+      "중고거래 platform ios app, 마이페이지, 지도, 필터, 검색 등을 맡아 구현",
+    img:
+      "https://user-images.githubusercontent.com/59603575/144080554-ffb9abfc-88e0-4201-aa05-35cf014f27fb.png",
+    gitUrl:
+      "https://apps.apple.com/kr/app/pampam/id1574847220?app=itunes&ign-mpt=uo%3D4",
+  },
+  {
+    title: "globlin",
+    info:
+      "React, Redux, Redux-saga를 이용하여 인플루언서 마케팅에 필요한 기능들을 구현",
+    img:
+      "https://user-images.githubusercontent.com/59603575/124921130-0bbfb880-e033-11eb-8cc3-ad051d3b24ac.png",
+    gitUrl: "https://github.com/Kwonkunkun/globlin_guide",
+  },
+  {
+    title: "simple_particle",
+    info: "particle 효과, 비 효과를 html canvas, 바닐라js로 구현",
+    img:
+      "https://user-images.githubusercontent.com/59603575/103346337-0e1ae600-4ad7-11eb-8b23-e5f618819610.gif",
+    gitUrl: "https://github.com/Kwonkunkun/Simple_particle",
+  },
+  {
+    title: "youtube_clone",
+    info: "Youtube api, react를 이용한 youtube 클론코딩 프로젝트",
+    img:
+      "https://user-images.githubusercontent.com/59603575/105004700-acb3ba80-5a77-11eb-90f5-d29bb14a529c.png",
+    gitUrl: "https://github.com/Kwonkunkun/React_youtube",
+  },
+  {
+    title: "cardmaker",
+    info: "react, firebase를 이용한 카드를 만들수 있는 프로젝트",
+    img:
+      "https://user-images.githubusercontent.com/59603575/105945809-5400a500-60a9-11eb-96f3-7c2363c35aad.gif",
+    gitUrl: "https://github.com/Kwonkunkun/React_cardmaker",
+  },
+  {
+    title: "온고지신-국궁(VR-BowAndArrow)",
+    info: "VR을 이용한 교육적 목적의 국궁 체험 시뮬레이션 콘텐츠",
+    img:
+      "https://user-images.githubusercontent.com/59603575/102003822-a9325400-3cbf-11eb-8367-af01291f9b33.png",
+    gitUrl: "https://github.com/Kwonkunkun/VR-BowAndArrow",
+  },
+  {
+    title: "VR-DrawAndPainting",
+    info:
+      "VR상에서 그림을 그려 딥러닝 모델로 판별하고 다시 VR환경에 관련 오브젝트를 띄운다. 이후, 오브젝트로 색칠하며 전시하는 프로젝트",
+    img:
+      "https://user-images.githubusercontent.com/59603575/102004661-a3d90780-3cc7-11eb-894c-fd19d713e5a1.png",
+    gitUrl: "https://github.com/Kwonkunkun/DrawAndPainting_VR",
+  },
+  {
+    title: "제3의 눈(AR 전시)",
+    info:
+      "서울문화재단 청년예술청 지원사업 - <스페이스랩:아직> 선정작품 , AR 전시 개발자로 참여, 뇌파센서와 연동하여 뇌파값에 따라 오브젝트 변화",
+    img:
+      "https://user-images.githubusercontent.com/59603575/102004337-9cfcc580-3cc4-11eb-93ce-d2bdaba81a20.png",
+    gitUrl: "https://github.com/Kwonkunkun/AR_Exhibition",
+  },
 ];
 
 const Project = () => {
-    const classes = useStyles();
-    return (
-        <section className={classes.Container}>
-            <Container
-                className={classes.cardGrid}
-                maxWidth="md"
-                component="main"
-            >
-                <Typography
-                    className={classes.title}
-                    component="h1"
-                    variant="h4"
-                    align="center"
-                    color="textPrimary"
-                    gutterBottom
-                >
-                    Projects
-                </Typography>
-                <Grid container spacing={4}>
-                    {projects.map((project, idx) => (
-                        <ProjectCard key={idx} project={project} />
-                    ))}
-                </Grid>
-            </Container>
-        </section>
-    );
+  const classes = useStyles();
+  return (
+    <section className={classes.Container}>
+      <Container className={classes.cardGrid} maxWidth="md" component="main">
+        <Typography
+          className={classes.title}
+          component="h1"
+          variant="h4"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Projects
+        </Typography>
+        <Grid container spacing={4}>
+          {projects.map((project, idx) => (
+            <ProjectCard key={idx} project={project} />
+          ))}
+        </Grid>
+      </Container>
+    </section>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({
-    Container: {
-        backgroundColor: "rgba(91,207,227,0.5)",
-        padding: theme.spacing(8, 0, 6),
-    },
-    cardGrid: {
-        paddingTop: theme.spacing(8),
-        paddingBottom: theme.spacing(8),
-    },
-    title: {
-        marginBottom: theme.spacing(10),
-    },
+  Container: {
+    backgroundColor: "rgba(91,207,227,0.5)",
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  title: {
+    marginBottom: theme.spacing(10),
+  },
 }));
 
 export default Project;
--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -6,60 +6,59 @@ import Grid from "@material-ui/core/Grid";
 import SkillDetail from "./skill-detail";
 
 const skills = [
-    { type: "C++", level: 70 },
-    { type: "JavaScript", level: 73 },
-    { type: "TypeScirpt", level: 65 },
-    { type: "Html", level: 75 },
-    { type: "Css", level: 60 },
-    { type: "emotion", level: 60 },
-    { type: "antd", level: 60 },
-    { type: "React", level: 80 },
-    { type: "Redux", level: 70 },
-    { type: "ReduxSaga", level: 70 },
-    { type: "ReactQuery", level: 50 },
+  { type: "JavaScript", level: 80 },
+  { type: "TypeScirpt", level: 80 },
+  { type: "Swift", level: 70 },
+  { type: "Html", level: 80 },
+  { type: "Css", level: 75 },
+  { type: "React", level: 90 },
+  { type: "React-native", level: 80 },
+  { type: "Redux", level: 80 },
+  { type: "ReduxSaga", level: 80 },
+  { type: "Recoil", level: 70 },
 ];
 
 const Skill = () => {
-    const classes = useStyles();
-    return (
-        <section className={classes.Container}>
-            <Container maxWidth="sm" component="main">
-                <Typography
-                    className={classes.title}
-                    component="h1"
-                    variant="h4"
-                    align="center"
-                    color="textPrimary"
-                    gutterBottom
-                >
-                    Skills
-                </Typography>
-                <Grid container spacing={4}>
-                    {/* {cards.map((card, idx) => (
+  const classes = useStyles();
+  return (
+    <section className={classes.Container}>
+      <Container maxWidth="sm" component="main">
+        <Typography
+          className={classes.title}
+          component="h1"
+          variant="h4"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Skills
+        </Typography>
+        <Grid container spacing={4}>
+          {/* {cards.map((card, idx) => (
                         <Grid key={idx} item sm={6} xs={12}>
                             <SkillCard key={idx} card={card} />
                         </Grid>
                     ))} */}
-                    <Grid item sm={12} xs={12}>
-                        <SkillDetail skills={skills} />
-                    </Grid>
-                </Grid>
-            </Container>
-        </section>
-    );
+          <Grid item sm={12} xs={12}>
+            <SkillDetail skills={skills} />
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({
-    Container: {
-        backgroundColor: "rgba(113,175,250,0.5)",
-        padding: theme.spacing(8, 0, 6),
-    },
-    title: {
-        marginBottom: theme.spacing(10),
-    },
-    skillDetail: {
-        marginTop: theme.spacing(3),
-    },
+  Container: {
+    backgroundColor: "rgba(113,175,250,0.5)",
+    padding: theme.spacing(8, 0, 6),
+  },
+  title: {
+    marginBottom: theme.spacing(10),
+  },
+  skillDetail: {
+    marginTop: theme.spacing(3),
+  },
 }));
 
 export default Skill;
